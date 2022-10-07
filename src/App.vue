3a5ef7a0fb7ad37bc95e13bc1f1d66bf28341a39
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2022-10-07 13:07:23 +0200

    upToDate

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 // Import your components here:
 import HelloJOHAK from "./components/HelloJOHAK.vue";
+import TodoList from "./components/TodoList.vue";
 </script>
 
 <template>
   <!-- Use your components here: -->
-  <HelloJOHAK></HelloJOHAK>
+  <TodoList></TodoList>
   <div>
     <p>
       built with <img src="/vite.svg" alt="vite-logo" /> &
---
+++ src/components/TodoList.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref, computed } from "vue";
+
+const list = ref([]);
+
+const newitem = ref("");
+const filter = ref("");
+
+function additem() {
+  list.value.push(newitem.value);
+  newitem.value = "";
+}
+
+function deleteitem(index) {
+  list.value.splice(index, 1);
+}
+</script>
+
+<!-- create a todolist with vue where i can delete items -->
+
+<template>
+  <div>
+    <input @keyup.enter="additem" type="text" v-model="newitem" />
+    <button @click="additem">Add Item</button><br />
+
+    <ul>
+      <li v-for="(item, index) in list" :key="item">
+        {{ item }}
+        <button @click="deleteitem(index)">X</button>
+      </li>
+    </ul>
+  </div>
+</template>
